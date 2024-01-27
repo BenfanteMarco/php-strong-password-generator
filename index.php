d3189127ca54c6b154eb7339010936646e9b1490
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-01-27 10:38:29 +0100

    scaffolding and form added

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>php-strong-password-generator</title>
+</head>
+<body>
+    <h2 class="p-3">Digita la lunghezza della password che desideri:</h2>
+    <div class="p-3">
+        <form action="./index.php" method="GET">
+            <label for="password_lenght">lunghezza Password:</label>
+            <input type="text" name="password_lenght" id="password_lenght">
+            <button class="mx-2" type="submit">Genera</button>
+        </form>
+    </div>
+</body>
+</html>
